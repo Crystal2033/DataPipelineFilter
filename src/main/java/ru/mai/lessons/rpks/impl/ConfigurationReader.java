227e28d6aff08da7a31c972070bc7559c8fa84ba
polmustdie <nesterovapolina77@gmail.com>
2023-03-17 09:08:38 +0300

    pol2

--- src/main/java/ru/mai/lessons/rpks/impl/ConfigurationReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/ConfigurationReader.java
@@ -7,6 +7,7 @@ public class ConfigurationReader implements ConfigReader {
     @Override
     public Config loadConfig() {
         String str;
+        String str1;
         return null; // написать код загрузки конфигураций сервиса фильтраций из файла *.conf
     }
 }
