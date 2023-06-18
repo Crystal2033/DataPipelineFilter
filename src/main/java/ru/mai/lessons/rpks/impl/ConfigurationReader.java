f1d8c81cbbb6b5ae6f2e2e6dee81d5cdda7c09a0
rahmatullintimur7777@gmail.com <rahmatullintimur7777@gmail.com>
2023-06-18 13:27:17 +0300

    initial commit

--- src/main/java/ru/mai/lessons/rpks/impl/ConfigurationReader.java
+++ src/main/java/ru/mai/lessons/rpks/impl/ConfigurationReader.java
@@ -1,11 +1,12 @@
 package ru.mai.lessons.rpks.impl;
 
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import ru.mai.lessons.rpks.ConfigReader;
 
 public class ConfigurationReader implements ConfigReader {
     @Override
     public Config loadConfig() {
-        return null; // написать код загрузки конфигураций сервиса фильтраций из файла *.conf
+        return ConfigFactory.parseResources("application.conf");
     }
 }
